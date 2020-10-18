73427b3690f1aa42ce0f1de6ea3dd7e7fd955ab5
VictorManuelTamayo <victortamayo509@gmail.com>
2020-10-18 00:22:30 -0500

    infiniteScroll

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 import { CarteleraResponse } from '../interfaces/cartelera-response';
 
 @Injectable({
@@ -8,10 +11,27 @@ import { CarteleraResponse } from '../interfaces/cartelera-response';
 })
 export class PeliculasService {
 
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage  =1;
+
   constructor( private http: HttpClient ) { }
+
+  get params(){
+    return{
+      api_key: '7f6cb9a8e6ca3fe40e67dd7e10524f72',
+      language: 'en-ES',
+      page: this.carteleraPage.toString()
+    }
+  }
 
   getCartelera():Observable<CarteleraResponse>{
 
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=7f6cb9a8e6ca3fe40e67dd7e10524f72&language=en-ES&page=1');
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing`,{
+      params: this.params
+    }).pipe(
+      tap( ()=>{
+        this.carteleraPage += 1;
+      })
+    );
   }
 }
