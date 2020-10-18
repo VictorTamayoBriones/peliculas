190ca8dbf99340cc2342ed81a9d46808c53b6946
VictorManuelTamayo <victortamayo509@gmail.com>
2020-10-17 23:34:34 -0500

    slider and poster

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import Swiper from 'swiper';
+
 import { Movie } from 'src/app/interfaces/cartelera-response';
 
 @Component({
@@ -6,16 +8,34 @@ import { Movie } from 'src/app/interfaces/cartelera-response';
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.scss']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
 
+  public mySwiper: Swiper;
+
   constructor() { }
+
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+    });
+
+
+  }
 
   ngOnInit(): void {
 
-    console.log(this.movies);
+    //console.log(this.movies);
 
   }
+  onSlideNext(){
+    this.mySwiper.slideNext();
+  }
+
+  onSlidePrev(){
+    this.mySwiper.slidePrev();
+  }
+
 
 }
