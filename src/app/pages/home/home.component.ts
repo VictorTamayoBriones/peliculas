24b506034a9fd98b34b478be0eb3b5446a312a9c
VictorManuelTamayo <victortamayo509@gmail.com>
2020-10-19 21:06:03 -0500

    resetAndSearch+

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 
@@ -7,7 +7,7 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy{
 
   public movies: Movie[]=[];
   public moviesSilideshow: Movie[]=[]
@@ -35,6 +35,10 @@ export class HomeComponent implements OnInit {
           this.movies = movies;
           this.moviesSilideshow = movies;
         })
+  }
+
+  ngOnDestroy(){
+    this.peliculasService.resetCarteleraPage();
   }
 
 }
